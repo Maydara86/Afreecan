579f0689678b8f4e9f03d7d3b45060dd08dce3ec
Unknown <cherif.mohamed86@gmail.com>
2018-03-28 18:35:57 +0100

    MVC for Child

---
+++ resources/views/children/create.blade.php
@@ -0,0 +1,33 @@
+@extends(layouts.master)
+
+@section('content')
+
+    <h1>Ajouter un Enfant</h1>
+
+    <hr>
+
+    <form method="POST" action="/posts">
+
+    {{ csrf_field() }}
+
+    <div class="form-group">
+        <label for="exampleInputtitle">Title</label>
+        <input type="text" class="form-control" id="exampleInputtitle" placeholder="Title" name="title" >
+    </div>
+
+    <div class="form-group">
+        <label for="exampleInputBody">Post</label>
+        <textarea class="form-control" id="exampleInputBody" rows="5" placeholder="Here you can type your post..." name="body" ></textarea>
+    </div>
+    
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary">Publish</button>
+    </div>
+
+    @if(count($errors))
+        @include('layouts.error')
+    @endif
+
+    </form>
+
+@endsection
